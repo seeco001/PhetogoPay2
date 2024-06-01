8779520c23a89c2254d4fc4c2925dc9d6fa86f11
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-01 09:12:56 +0200

    Add function: createWallet

--- amplify/functions/createWallet/resource.ts
+++ amplify/functions/createWallet/resource.ts
@@ -1,6 +1,6 @@
 import { defineFunction } from '@aws-amplify/backend';
 
-export const sayHello = defineFunction({
+export const createWallet = defineFunction({
   // optionally specify a name for the Function (defaults to directory name)
   name: 'createWallet',
   // optionally specify a path to your handler (defaults to "./handler.ts")
