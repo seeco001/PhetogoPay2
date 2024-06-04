0a9cb4576a017b2255d2bb3cf1609691b3a1d6eb
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-04 11:59:39 +0200

    Create user profile record

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,10 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { createWallet } from './functions/createWallet/resource'
 
 defineBackend({
   auth,
   data,
-  createWallet
 });
