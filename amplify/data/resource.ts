b6f0b6a85303b818e40bc54dd1c6c810edfade49
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-04 11:26:17 +0200

    Create user profile record

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,7 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 import { postConfirmation } from "../auth/post-confirmation/resource";
-/*== STEP 1 ===============================================================
-The section below creates a Todo database table with a "content" field. Try
-adding a new "isDone" field as a boolean. The authorization rule below
-specifies that any user authenticated via an API key can "create", "read",
-"update", and "delete" any "Todo" records.
-=========================================================================*/
+
+
 const schema = a
     .schema({
         UserProfile: a
