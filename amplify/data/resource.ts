021b959e4b4964511e6e061ef71e444e845075e8
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-04 10:47:36 +0200

    Add function: createWallet

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,26 +1,37 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
-
+import { postConfirmation } from "../auth/post-confirmation/resource";
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
 adding a new "isDone" field as a boolean. The authorization rule below
 specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
-const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-      isDone: a.boolean()
+const schema = a
+    .schema({
+        UserProfile: a
+            .model({
+                email: a.string(),
+                profileOwner: a.string(),
+                walletAccount: a.string(),
+                creditAccount: a.string(),
+                name: a.string(),
+                surname: a.string(),
+                govId: a.string(),
+                address: a.string(),
+                contacts: a.string(),
+                netIncome: a.string(),
+                disIncome: a.string(),
+                localExpense: a.string(),
+                signDeclaration: a.boolean()
+            })
     })
-    .authorization((allow) => [allow.owner()]),
-});
-
+    .authorization((allow) => [allow.resource(postConfirmation)]);
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "userPool",
+    defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
