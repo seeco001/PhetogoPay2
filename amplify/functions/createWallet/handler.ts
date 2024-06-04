f3b3256134cac3d9b016204ab8cd9368d904bce2
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-04 11:57:56 +0200

    Create user profile record

--- amplify/functions/createWallet/handler.ts
+++
@@ -1,6 +0,0 @@
-import { Handler } from 'aws-lambda';
-
-export const handler: Handler = async (event, context) => {
-  // your function code goes here
-  return 'Hello, World!';
-};
--- amplify/functions/createWallet/resource.ts
+++
@@ -1,8 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-
-export const createWallet = defineFunction({
-  // optionally specify a name for the Function (defaults to directory name)
-  name: 'createWallet',
-  // optionally specify a path to your handler (defaults to "./handler.ts")
-  entry: './handler.ts'
-});
