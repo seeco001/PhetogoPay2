72e1344a6d28112deb6623c10785173f2ba9f93d
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-17 17:13:10 +0200

    Build Banking

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -79,7 +79,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
