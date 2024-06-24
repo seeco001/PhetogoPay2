21e5ae257233b8e773d6a42a450e25e7e4010b28
Itumeleng Seeco <seeco.itu@gmail.com>
2024-06-24 16:49:37 +0200

    Banking Updated

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -82,7 +82,7 @@ const schema = a
             })
             .authorization((allow) => [
                 allow.owner()
-                    .to(['read']),
+                    .to(['read','create']),
 
             ]),
 
@@ -105,7 +105,7 @@ const schema = a
             })
             .authorization((allow) => [
                 allow.owner()
-                    .to(['read']),
+                    .to(['read','create']),
             ]),
     }).authorization(allow => [allow.owner()]);
 
